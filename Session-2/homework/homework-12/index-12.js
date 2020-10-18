940180b145a398543eb80aebc5665e87ecf91ad6
Minh-Tam27 <tamnm2828@gmail.com>
2020-10-18 12:09:14 +0700

    add full hw

---
+++ Session-2/homework/homework-12/index-12.js
@@ -0,0 +1,9 @@
+console.log ('Ex 12:');
+console.log ('let n = prompt ( ' + `${'Enter n:'}` + ' ); ' );
+console.log ('{');
+console.log ('for (var i=1; i <= n; i++)');
+console.log ('{');
+console.log ('fd(50)');
+console.log ('lt(360/n)');
+console.log ('}');
+console.log ('}');
---
+++ Session-2/index.js
@@ -0,0 +1,13 @@
+let age = prompt('Your age?');
+if (age < 10)
+{
+    alert ('Stay at home');
+}
+else if (age < 18)
+{
+    alert ('Eat ice cream');
+}
+else
+{
+    alert ('Go to cinema');
+}
