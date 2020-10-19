cff12bdc0f0b4b90462a57713bb5ac93fcd8491c
Minh-Tam27 <tamnm2828@gmail.com>
2020-10-19 12:18:23 +0700

    add full homework

---
+++ Session-2/homework/homework-13/index-13.js
@@ -0,0 +1,10 @@
+console.log ('Ex 13:');
+console.log ('let n = prompt ( ' + `${'Enter n:'}` + ' ); ' );
+console.log ('{');
+console.log ('for (var m=1; m<=n; m++)');
+console.log ('for (var i=1; i <= n; i++)');
+console.log ('{');
+console.log ('fd(50)');
+console.log ('lt(360/n)');
+console.log ('}');
+console.log ('}');
