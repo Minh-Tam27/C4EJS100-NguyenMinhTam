d64d056a84cf5945afa748778e85af36aef2148d
Minh-Tam27 <tamnm2828@gmail.com>
2020-10-23 19:02:17 +0700

    add

--- Session-3/homework/homework-4/hw-4.js
+++ Session-3/homework/homework-4/hw-4.js
@@ -1,43 +1,44 @@
 console.log('Ex 4:');
 let shop = ['Jean', 'T-Shirt', 'Socks'];
+console.log(shop);
+let choose;
 while(true)
 {
-    let choose = String(prompt('Hi there, welcome to shop, what do you want (C,R,U,D)'));
-    if(choose.toLowerCase() != 'c' && choose.toLowerCase() != 'r' && choose.toLowerCase() != 'u' && choose.toLowerCase() != 'd')
+    choose = prompt('Hi there, welcome to shop, what do you want (C,R,U,D)');
+    if (choose == '' || choose == null)
     {
-        alert('This command is not supported');
         break;
     }
     else
     {
-        if (choose.toLowerCase()=='r')
-        {
+    switch (choose)
+    {
+        case 'r':
             console.log ('The current items are:')
             for (let i=0; i<shop.length; i++)
             {
                 console.log(`${i+1}`+ '. ' + `${shop[i]}`);
             }
-        }
-        else if (choose.toLowerCase()=='c')
-        {
+            break;
+        case 'c':
             let newItem = prompt('Enter name of new item:');
             shop.push(newItem);
             alert('Done');
-        }
-        else if (choose.toLowerCase()=='u')
-        {
+            break;
+        case 'u':
             let u = Number(prompt('Enter the position of item you wanna update:'));
             let itemUpdate = prompt('Enter new name:');
-            shop[u-1]=itemUpdate;
+            shop[u-1] = itemUpdate;
             alert('Done');
-        }
-        else if (choose.toLowerCase()=='d')
-        {
+            break;
+        case 'd':
             let itemDelete = Number(prompt('Enter the position of item you wanna update:'));
             shop.splice(itemDelete-1,1);
             alert('Done');
-        }
+            break;
+        default:
+            alert('This command is not supported');
+            break;
     }
-}  
-
-
+    }
+}
