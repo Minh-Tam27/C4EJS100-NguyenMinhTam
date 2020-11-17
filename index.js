fb83e6ffc49a1b99376a618f7cab57152c73c30d
Minh-Tam27 <tamnm2828@gmail.com>
2020-11-17 13:58:53 +0700

    add

---
+++ index.js
@@ -0,0 +1,16 @@
+//function 2 tham số
+function add(a, b) {
+    return a+b;
+}
+console.log("The result of sum between one and two is ", add(1, 2));
+
+console.log("Hello, we are One and Two, our sum is ", add(1, 2));
+//function 1 tham số
+function hi(name)
+{
+    alert('Hi ' + name);
+    alert(`${name} is great`);
+}
+
+hi('Tâm');
+//tham số: name; đối số: Tâm
